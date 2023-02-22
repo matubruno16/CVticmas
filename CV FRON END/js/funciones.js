c468165fccba60380e7c372e5d66da8d588b7e97
Matias Bruno <112434749+matubruno16@users.noreply.github.com>
2023-02-22 10:24:54 -0300

    Add files via upload

---
+++ CV FRON END/js/funciones.js
@@ -0,0 +1,6 @@
+
+document.getElementById('botonjs').onclick = function () {
+    console.log("se registro su consulta");
+    document.getElementById('texto').innerHTML = "Su consulta fue enviada correctamente. Nos contactaremos lo mas pronto posible.";
+}
+
